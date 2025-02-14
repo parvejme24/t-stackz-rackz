ae3fcd4ca81516fdd9af5c42417dd129b3c5cf80
Foysal79 <onthor312001@gmail.com>
2025-02-14 22:28:55 +0600

    privacyPolicy part completed with responsivenes

--- src/components/custom/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/components/custom/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,3 +1,17 @@
-export default function PrivacyPolicy() {
-  return <div>PrivacyPolicy</div>;
-}
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return <div className="bg-[#222629] ">
+    <div className=" w-11/12 md:w-10/12 mx-auto py-[20px] md:py-[40px]">
+      
+        <h1 className="text-center font-semibold text-base md:text-4xl pb-[24px] md:pb-[40px]">Privacy Policy</h1>
+        <p className="text-justify font-normal text-xs md:text-xl">Your privacy matters to us at T-Stackz Rackz. We collect minimal data to provide secure trading services. Your information is encrypted, never shared, and protected with 2FA & advanced security measures. <br />
+We use your data to enhance platform security, improve user experience, and comply with regulations. We do not sell or share your personal information with third parties. All transactions and personal data are safeguarded with end-to-end encryption and real-time fraud detection. <br />
+You have full control over your data and can request access, updates, or deletion at any time. By using our platform, you agree to our data policies.</p>
+    </div>
+  </div>;
+};
+
+export default PrivacyPolicy;
