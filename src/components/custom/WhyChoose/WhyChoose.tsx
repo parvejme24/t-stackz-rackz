43b0f30a260b991078df3652e67bf02c585df924
Foysal79 <onthor312001@gmail.com>
2025-02-16 00:59:02 +0600

    why choose t stacks reacks session completed

---
+++ src/components/custom/WhyChoose/WhyChoose.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable prettier/prettier */
+
+
+import bgImage from "../../../../public/Frame 167.png";
+
+
+const WhyChoose = () => {
+    return (
+        <div className="container mx-auto max-w-7xl">
+  <div 
+    className="w-full h-[500px] sm:h-[700px] md:h-[900px] lg:h-[1000px] bg-cover sm:bg-contain bg-no-repeat bg-center"
+    style={{ backgroundImage: `url(${bgImage})` }}
+  >
+  </div>
+</div>
+
+    );
+};
+
+export default WhyChoose;
