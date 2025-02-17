61a85011d314420f609ea3cab827c62a24328eb9
Foysal79 <onthor312001@gmail.com>
2025-02-17 13:04:17 +0600

    learn about trading session aos animition implimention

--- src/components/custom/AboutTrading/AboutTrading.tsx
+++ src/components/custom/AboutTrading/AboutTrading.tsx
@@ -8,6 +8,9 @@ import B2 from "/public/images/b2.png";
 import B3 from "/public/images/b3.png";
 
 import USERPROFILE from "/public/images/user_profile.png";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const cards = [
   {
@@ -37,19 +40,28 @@ const cards = [
 ];
 
 export default function AboutTrading() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Initialize AOS with a duration of 1000ms
+  }, []);
   return (
     <div className="bg-[#0D0D0D]">
       <div className="container mx-auto max-w-7xl px-6 py-5 md:py-20">
-        <h1 className="text-center font-semibold text-base md:text-4xl pb-6 md:pb-10 text-[#C0C0C0]">
+
+        <h1  
+        data-aos="fade-up"
+         className="text-center font-semibold text-base md:text-4xl pb-6 md:pb-10 text-[#C0C0C0]">
           Learn About Trading
         </h1>
-        <div className="pb-6 md:pb-10 grid grid-cols-1 md:grid-cols-2 gap-5">
+
+        <div 
+        data-aos="fade-up"
+         className="pb-6 md:pb-10 grid grid-cols-1 md:grid-cols-2 gap-5">
           <img className="w-full" src={ABOUT_IMAGE} alt="" />
           <div className="flex-1">
-            <h1 className="font-medium text-xl lg:text-3xl text-[#00F5FF] pb-3 lg:pb-6">
+            <h1  data-aos="fade-up" className="font-medium text-xl lg:text-3xl text-[#00F5FF] pb-3 lg:pb-6">
               Master the Markets: Trade Smarter with T-Stackz Rackz
             </h1>
-            <p className="font-normal text-sm md:text-base text-[#C0C0C0] pb-5 lg:pb-8">
+            <p data-aos="fade-up" className="font-normal text-sm md:text-base text-[#C0C0C0] pb-5 lg:pb-8">
               Unlock the secrets of successful trading with T-Stackz Rackz!
               Whether you're new to stocks, options, or crypto, or an
               experienced trader looking for an edge, our platform provides
@@ -57,14 +69,16 @@ export default function AboutTrading() {
               help you trade smarter and profit faster. Watch this video to see
               how T-Stackz Rackz can transform your trading journey!
             </p>
-            <button className="flex justify-center items-center gap-3 text-[#FFFFFF] text-xs md:text-base font-normal py-2 md:py-3 px-4 md:px-10 bg-[#FD5163] rounded-xl">
+            <button data-aos="fade-up" className="flex justify-center items-center gap-3 text-[#FFFFFF] text-xs md:text-base font-normal py-2 md:py-3 px-4 md:px-10 bg-[#FD5163] rounded-xl">
               Watch Video <GoArrowRight />
             </button>
           </div>
         </div>
-        <div className="grid  grid-cols-1 md:grid-cols-3 gap-6 ">
+        <div  className="grid  grid-cols-1 md:grid-cols-3 gap-6 ">
           {cards.map((card, index) => (
-            <AboutTradingCard key={index} {...card} />
+            <div key={index} data-aos="fade-up">
+              <AboutTradingCard  {...card} />
+            </div>
           ))}
         </div>
       </div>
