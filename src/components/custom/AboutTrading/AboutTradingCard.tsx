4a4da6a0c7f1d2f3b84e5652890aece51e71c47a
Md Parvej <mdparvejme24@gmail.com>
2025-03-30 13:55:11 +0600

    update some section

--- src/components/custom/AboutTrading/AboutTradingCard.tsx
+++ src/components/custom/AboutTrading/AboutTradingCard.tsx
@@ -18,23 +18,31 @@ const AboutTradingCard: React.FC<CardProps> = ({
   readTime,
 }) => {
   return (
-    <div className="bg-[#222629] rounded-3xl   p-4 ">
-      <img alt={title} className="rounded-xl w-full " src={image} />
-      <h2 className="text-lg font-semibold mt-3 text-[#00F5FF]">{title}</h2>
+    <div className="">
+      <img
+        alt={title}
+        className="rounded-xl w-full "
+        src={image}
+        draggable="false"
+      />
+      <div className="bg-[#222629] p-4 -mt-5 rounded-lg">
+        <h2 className="text-lg font-semibold mt-5 text-[#00F5FF]">{title}</h2>
 
-      <div className="flex items-center mt-3">
-        <img
-          alt={authorName}
-          className="w-10 h-10 rounded-full"
-          src={authorImage}
-        />
-        <div className="ml-3">
-          <p className="font-semibold text-[#00F5FF]">{authorName}</p>
-          <div className=" flex items-center gap-5">
-            <p className="text-[#C0C0C0] text-sm ">{date} </p>
-            <div className="flex items-center gap-1">
-              <p className="text-[#C0C0C0] text-xl ">• </p>
-              <p className="text-[#C0C0C0] text-sm ">{readTime}</p>
+        <div className="flex items-center mt-3">
+          <img
+            alt={authorName}
+            className="w-10 h-10 rounded-full"
+            src={authorImage}
+            draggable="false"
+          />
+          <div className="ml-3">
+            <p className="font-semibold text-[#00F5FF]">{authorName}</p>
+            <div className=" flex items-center gap-5">
+              {/* <p className="text-[#C0C0C0] text-sm ">{date} </p> */}
+              <div className="flex items-center gap-1">
+                {/* <p className="text-[#C0C0C0] text-xl ">• </p> */}
+                {/* <p className="text-[#C0C0C0] text-sm ">{readTime}</p> */}
+              </div>
             </div>
           </div>
         </div>
