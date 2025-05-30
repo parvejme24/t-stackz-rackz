51fcc42f3190edf18446afb17c4288df47931388
Md Parvej <mdparvejme24@gmail.com>
2025-05-30 21:54:17 +0600

    update banner and footer link

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
 
-function App() {
+export default function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
     </Routes>
   );
 }
-
-export default App;
